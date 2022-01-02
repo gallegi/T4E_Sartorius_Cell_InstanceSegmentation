b097ea46537fe411f3f3eea2ddbd44e54312ad6b
Nguyen The Nam <namnguyen61031@gmail.com>
2022-01-02 11:19:35 +0700

    Add cmd to extract data

--- download_data.sh
+++ download_data.sh
@@ -1,1 +1,3 @@
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Sssu-cXxgdPmlyaP1JQ7N2BxnK8kmCb1' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Sssu-cXxgdPmlyaP1JQ7N2BxnK8kmCb1" -O data/annotation_semisupervised_round2.tar.gz && rm -rf /tmp/cookies.txt
+
+tar -xvf data/annotation_semisupervised_round2.tar.gz -C data/
