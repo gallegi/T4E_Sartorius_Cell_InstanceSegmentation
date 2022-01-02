faf9be38b7a4cc6d960c543ad8638ae26ba0338f
Nguyen The Nam <namnguyen61031@gmail.com>
2022-01-02 11:40:56 +0700

    Fix syntax error in install_libs.sh

--- install_libs.sh
+++ install_libs.sh
@@ -1,3 +1,6 @@
-!pip install torch==1.10.1+cu113 torchvision==0.11.2+cu113 torchaudio==0.10.1+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
-!python -m pip install 'git+https://github.com/zhanghang1989/detectron2-ResNeSt.git'
-!pip install -r requirements.txt
+# 1. install pytorch, you might have to change the above line to make it compatible with your cuda version
+pip install torch==1.10.1+cu113 torchvision==0.11.2+cu113 torchaudio==0.10.1+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
+# 2. install detectron2
+python -m pip install 'git+https://github.com/zhanghang1989/detectron2-ResNeSt.git'
+# 3. install other dependencies
+pip install -r requirements.txt
