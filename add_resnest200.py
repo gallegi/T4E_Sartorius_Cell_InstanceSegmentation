63db8b9b9f42dc739daa0a08b76b5533f5b87a6f
Nguyen The Nam <namnguyen61031@gmail.com>
2022-01-04 22:11:46 +0700

    Remove ls cmd

--- add_resnest200.py
+++ add_resnest200.py
@@ -7,5 +7,3 @@ DEST_FOLDER = os.path.dirname(detectron2.__file__)
 shutil.copy('resnest200/resneSt.py', f'{DEST_FOLDER}/modeling/backbone/')
 shutil.copy('resnest200/splat.py', f'{DEST_FOLDER}/modeling/backbone/')
 shutil.copy('resnest200/fpn_resneSt.py', f'{DEST_FOLDER}/modeling/backbone/')
-
-print(os.cmd(f'ls {DEST_FOLDER}'))
