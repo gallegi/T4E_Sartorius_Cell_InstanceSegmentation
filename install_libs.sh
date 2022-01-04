e9473ad28165a51fee06937cad6f9e24f85b8a1a
Nguyen The Nam <namnguyen61031@gmail.com>
2022-01-04 22:09:14 +0700

    install torch 1.10.1

--- install_libs.sh
+++ install_libs.sh
@@ -1,5 +1,5 @@
 # 1. install pytorch, you might have to change the above line to make it compatible with your cuda version
-pip install torch==1.10.0+cu111 torchvision==0.11.2+cu111 torchaudio==0.10.1+cu111 -f https://download.pytorch.org/whl/cu111/torch_stable.html
+pip install torch==1.10.1+cu111 torchvision==0.11.2+cu111 torchaudio==0.10.1+cu111 -f https://download.pytorch.org/whl/cu111/torch_stable.html
 
 # 2. install detectron2
 # python -m pip install 'git+https://github.com/zhanghang1989/detectron2-ResNeSt.git'
@@ -10,4 +10,4 @@ python -m pip install  --upgrade detectron2 -f https://dl.fbaipublicfiles.com/de
 pip install -r requirements.txt
 
 # 4. Make detectron2 works with ResNeSt200
-python add_resnest200.py
+python add_resnest2.py
