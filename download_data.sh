2f03c882000ad398afa53b80b4221473914fcc52
Nguyen The Nam <namnguyen61031@gmail.com>
2022-01-09 21:54:56 +0700

    Change script to optimizing data downloading

--- download_data.sh
+++ download_data.sh
@@ -1,3 +1,8 @@
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Sssu-cXxgdPmlyaP1JQ7N2BxnK8kmCb1' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Sssu-cXxgdPmlyaP1JQ7N2BxnK8kmCb1" -O data/annotation_semisupervised_round2.tar.gz && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1M7C6FHgBnCjxo7qTmz3Zn0tHTwznEEGy' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1M7C6FHgBnCjxo7qTmz3Zn0tHTwznEEGy" -O data/annotation_semisupervised_round1.zip && rm -rf /tmp/cookies.txt
+unzip data/annotation_semisupervised_round1.zip -d data/
 
-tar -xvf data/annotation_semisupervised_round2.tar.gz -C data/
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1coW2dINEvQba50vAaOSvURCQa_8L4CHR' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1coW2dINEvQba50vAaOSvURCQa_8L4CHR" -O data/annotation_semisupervised_round2.zip && rm -rf /tmp/cookies.txt
+unzip data/annotation_semisupervised_round2.zip -d data/
+
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=133nHthlCY-qnnXG35GQU8iyoxgoPM7bP' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=133nHthlCY-qnnXG35GQU8iyoxgoPM7bP" -O data/images.zip && rm -rf /tmp/cookies.txt
+unzip data/images.zip -d data/
